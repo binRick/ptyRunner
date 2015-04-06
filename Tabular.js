a964250226cfcba310216e0ff5d7420ba2fa4e5b
binRick <rapidvps@gmail.com>
2015-04-06 17:59:31 -0400

    tablular ideas

---
+++ Tabular.js
@@ -0,0 +1,51 @@
+var Table = require("terminal-table"),
+    clear = require('clear'),
+    _ = require('underscore');
+
+
+
+module.exports = function(Columns, Rows, Attrs, Interval) {
+
+    setInterval(function() {
+        var t = new Table({
+            borderStyle: 2,
+            horizontalLine: true,
+            width: [3, "10%", "10%", "10%", "10%", "10%"],
+            rightPadding: 0,
+            leftPadding: 1
+        });
+        t.push(Columns);
+        _.each(Rows, function(r) {
+            t.push(r);
+        });
+
+        _.each(Attrs, function(a) {
+
+            //       t.attrRange(a);
+        });
+
+        t.attrRange({
+            row: [0, 1]
+        }, {
+            align: "center",
+            color: "green",
+            bg: "black"
+        });
+
+        t.attrRange({
+            column: [0, 1]
+        }, {
+            color: "green"
+        });
+
+        t.attrRange({
+            row: [1],
+            column: [1]
+        }, {
+            leftPadding: 5
+        });
+
+        clear();
+        console.log("" + t);
+    }, Interval);
+};
---
+++ drawTable.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var Tabular = require('./Tabular');
+
+var Cols = ["", "Application", "User", "Service", "Process", "Status"];
+var Attrs = [];
+Attrs.push({
+    row: [0, 1]
+}, {
+    align: "center",
+    color: "green",
+    bg: "black"
+});
+
+var Rows = [];
+Rows.push(["✓", "Arma3Server", "arma3server", "tty.js", "xxxx", "xxx"]);
+Rows.push(["x", "Arma3Server", "arma3server", "tty.js", "xxxx", "xxx"]);
+
+Tabular(Cols, Rows, Attrs, 3000);
