6c9833c4c61cd0b514963b411553777d0b12f01c
binRick <rapidvps@gmail.com>
2015-06-17 22:27:05 -0400

    new

--- zfsSnapMBuffer.js
+++ zfsSnapMBuffer.js
@@ -16,6 +16,25 @@ program.version('0.0.1')
     .option('-l, --limit [limit]', 'Display Limit')
     .parse(process.argv);
 
+var HandleMbufferStdErr = function(str) {
+    str = str.toString('utf8');
+
+
+    if (str.split('Address already in use') > 0) {
+        console.log(c.red.bgBlack('Address is in use on remote'));
+    }
+    return console.log(c.green.bgBlack(str));
+    err = trim(String(str));
+    var aP = str.split(' ');
+    var strO = {
+        inRate: aP[2] + ' ' + a[3], //.replace(/,/,''),
+        outRate: aP[6] + ' ' + a[7], //.replace(/,/,''),
+        total: aP[8] + ' ' + a[9],
+        bufferPercentageFull: aP[12],
+    };
+    console.log(c.red.bgBlack(str));
+};
+
 var cmd = 'ls -al /root';
 var server = 'beta';
 var callback = function(e, s) {
@@ -23,7 +42,7 @@ var callback = function(e, s) {
     console.log(s);
 };
 
-var zfsRecvFlags = '-vF';
+var zfsRecvFlags = '-F';
 var zfsDest = 'tank/sRick/1';
 
 var RemotePort = 9092;
@@ -34,7 +53,7 @@ var mArray = ['mbuffer', '-s', '128k', '-m', '512M', '-O', server + ':' + Remote
 var Commands = {
     //    Local: 'zfs send ' + Snap + ' | mbuffer -s 128k -m 512M -O ' + server + ':' + RemotePort,
     Local1: 'zfs send tank/Rick@zfs-auto-snap-2015-06-17-1820 | mbuffer -s 128k -m 1G -O beta:9092',
-    Local: 'zfs send tank/Rick@zfs-auto-snap-2015-06-17-1820',
+    Local: String('zfs send tank/Rick@zfs-auto-snap-2015-06-17-1820').split(' '),
     Remote: 'mbuffer -s 128k -m 512M -I ' + RemotePort + ' | zfs recv ' + zfsRecvFlags + ' ' + zfsDest,
 };
 if (program.debug) {
@@ -42,7 +61,7 @@ if (program.debug) {
     console.log(pj.render(Commands));
     process.exit();
 }
-
+/*
 zfs.list(function(err, fields, data) {
     if (err) throw err;
     var data_s = _.sortBy(data.map(function(m) {
@@ -56,103 +75,63 @@ zfs.list(function(err, fields, data) {
         m.remote = m.name.split('/')[2];
         return m;
     }).reverse().slice(0, program.limit || 4);
-
-    if (program.debug)
-        console.log(pj.render(data_s, {
-            inlineArrays: true
-        }));
-
-    if (program.table) {
-        var Tabular = require('./myTabular');
-        var Cols = ["", "Filesystem", "Used Bytes", 'Remote', 'Connection', 'Filesystems'];
-        var Attrs = [];
-        Attrs.push({
-            row: [0, 1]
-        }, {
-            align: "center",
-            color: "green",
-            bg: "black"
+*/
+var conn = new Client();
+var start = new Date().getTime();
+console.log(c.red('waiting for ssh'));
+conn.on('ready', function() {
+    var data = '';
+    console.log(c.yellow('ready'));
+    setTimeout(function() {
+        console.log(c.green('launching'));
+        var mbufferSend = child_process.spawn('mbuffer', ['-s', '128k', '-m', '512M', '-O', server + ':' + RemotePort]);
+        var zfsSend = child_process.spawn(Commands.Local[0], Commands.Local.slice(1));
+        zfsSend.stdout.pipe(mbufferSend.stdin);
+        mbufferSend.stdout.on('data', function(data) {
+            console.log(data.toString("utf8"));
+        });
+        zfsSend.on('close', function(e) {
+            console.log('zfs send process exited with code', c.green(e));
+        });
+        zfsSend.on('error', function(e) {
+            console.log(c.red('zfs send err', e));
+        });
+        mbufferSend.on('error', function(e) {
+            console.log('mbuf send error', e);
+        });
+        mbufferSend.on('close', function(e) {
+            console.log('mbuf send close', e);
         });
 
-        var Rows = [];
-        _.each(data_s, function(d) {
-            Rows.push(["✓", d.name, d.used, d.remote]);
-
+    }, 1000);
+    conn.exec(Commands.Remote, function(err, stream) {
+        if (err) throw err;
+        stream.on('data', function(data) {
+            data = data.toString('utf8');
+            console.log(c.red.bgWhite(data));
         });
-        Tabular(Cols, Rows, Attrs, 10000);
-    }
-    var conn = new Client();
-    var start = new Date().getTime();
-    console.log(c.red('waiting for ssh'));
-    conn.on('ready', function() {
-        var data = '';
-        console.log(c.yellow('ready'));
-        setTimeout(function() {
-            console.log(c.green('launching'));
-            var A = Commands.Local.split(' ')[0];
-            var B = Commands.Local.split(' ').slice(1);
-            //            console.log(A);
-            //            console.log(B);
-            //            process.exit();
-            var mbufferSend = child_process.spawn('mbuffer', ['-s', '128k', '-m', '512M', '-O', server+':'+RemotePort]);
-//            var mbufferSend = child_process.spawn('mbuffer', ['-s', '128k', '-m', '512M', '-O', server, '-i', '-']);
-            var zfsSend = child_process.spawn(A, B);
-            zfsSend.stdout.pipe(mbufferSend.stdin);
-mbufferSend.stdout.on('data', function(data) { 
-  console.log(data.toString("utf8")); 
-});
-/*            zfsSend.on('close', function(e) {
-                console.log('zfs send close', e);
+        stream.on('close', function(code, signal) {
+            conn.end();
+        }).on('data', function(data) {
+            data = trim(data.toString());
+            //                                if (typeof(Command.process) == 'function')
+            //                                    data = Command.process(data);
+            callback(null, {
+                server: server,
+                cmd: cmd,
+                //                                    key: Command.key,
+                //                                    title: Command.title,
+                started: start,
+                millisecs: new Date().getTime() - start,
+                ts: new Date().getTime(),
+                data: data,
             });
-            zfsSend.on('error', function(e) {
-                console.log('zfs send error', e);
-            });
-            mbufferSend.on('error', function(e) {
-                console.log('mbuf send error', e);
-            });
-            mbufferSend.on('close', function(e) {
-                console.log('mbuf send close', e);
-            });*/
-            /*            zfsSend.stdout.on('data', function(data) {
-                            data = trim(data.toString());
-                            console.log(c.green(data));
-                        });
-                        zfsSend.stderr.on('data', function(data) {
-                            data = trim(data.toString());
-                            console.log(c.red(data));
-                        });
-                        zfsSend.on('close', function(code, sig) {
-                            console.log(c.green('send closed w'), c.yellow(code));
-                        });
-            */
-        }, 1000);
-        conn.exec(Commands.Remote, function(err, stream) {
-            if (err) throw err;
-            stream.on('close', function(code, signal) {
-                conn.end();
-            }).on('data', function(data) {
-                data = trim(data.toString());
-                //                                if (typeof(Command.process) == 'function')
-                //                                    data = Command.process(data);
-                callback(null, {
-                    server: server,
-                    cmd: cmd,
-                    //                                    key: Command.key,
-                    //                                    title: Command.title,
-                    started: start,
-                    millisecs: new Date().getTime() - start,
-                    ts: new Date().getTime(),
-                    data: data,
-                });
-            }).stderr.on('data', function(data) {
-                console.log(c.red.bgWhite(data));
-                //                            callback(data, null);
-            });
-        });
-    }).connect({
-        host: server,
-        port: 22,
-        username: 'root',
-        privateKey: require('fs').readFileSync('/root/.ssh/id_rsa')
+        }).stderr.on('data', HandleMbufferStdErr);
     });
+}).connect({
+    host: server,
+    port: 22,
+    username: 'root',
+    privateKey: require('fs').readFileSync('/root/.ssh/id_rsa')
 });
+//});
